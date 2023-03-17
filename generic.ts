4f9981052dd5f1f8dbfcf5e9de2bcba511258c31
Tran Viet Quang <quangtvph25239@fpt.edu.vn>
2023-03-17 12:40:09 +0700

    Update generic.ts

--- generic.ts
+++ generic.ts
@@ -1,25 +1,17 @@
-const numArray10 = [5,2,6,8,3,12]
-const numArray12 = ["c", "a", "b","d"]
-function selectionSort1<T>(arr: T[], callback: ((a: T, b: T) => number)): T[] {
-  const len = arr.length;
-  for (let i = 0; i < len - 1; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < len; j++) {
-      if (callback(arr[j], arr[minIndex])<0) {
-        minIndex = j;
-      }
+const numArray = [5,2,6,8,3,12]
+const numArray1 = ["Tran", "Viet", "Quang"]
+function selectionSort<T>(arr: T[]): T[] {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];          
+            }
+        }
     }
-    if (minIndex !== i) {
-      let temp = arr[i]
-      arr[i] = arr[minIndex]
-      arr[minIndex] = temp
-    }
-  }
-  return arr;
+
+    return arr;
 }
-const result4 = selectionSort1(numArray10, (a,b)=>{
-    return a-b
-}) 
-const result5 = selectionSort1(numArray12, (a, b) => a.localeCompare(b));
-console.log(result4);
-console.log(result5);
+
+selectionSort(numArray)
+
+console.log(numArray);
